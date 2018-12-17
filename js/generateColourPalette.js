13487e0e9b17e69f44d293cdcd2035708ce4ef72
AlexanderMurie <alexandermurie@gmail.com>
2018-12-17 23:52:18 +0200

    Modal structure figured out; random colour palette idea abandoned

---
+++ js/generateColourPalette.js
@@ -0,0 +1,45 @@
+
+
+
+/*
+window.onload = function() {
+
+console.log(Math.floor(Math.random() * 5));
+
+
+	switch (Math.floor(Math.random() * 3)) {
+		case 0:
+			
+			document.documentElement.style.setProperty('--light-shade','#B2F7EF');
+			document.documentElement.style.setProperty('--medium-shade','#7BDFF2');
+			document.documentElement.style.setProperty('--final-shade','#EFF7F6');
+			document.documentElement.style.setProperty('--text-soft-white','#EFF7F6');
+			document.documentElement.style.setProperty('--grey', '#696969');
+			document.documentElement.style.setProperty('--white','#FFFFFF');
+			document.documentElement.style.setProperty('--black','#000000');
+			document.documentElement.style.setProperty('--img','url("paint-pink-compressed.jpg")');
+		case 1:
+			
+			document.documentElement.style.setProperty('--light-shade','#FFBF69');
+			document.documentElement.style.setProperty('--medium-shade','#FF9F1C');
+			document.documentElement.style.setProperty('--final-shade','#CBF3F0');
+			document.documentElement.style.setProperty('--text-soft-white','#EFF7F6');
+			document.documentElement.style.setProperty('--grey', '#696969');
+			document.documentElement.style.setProperty('--white','#FFFFFF');
+			document.documentElement.style.setProperty('--black','#000000');
+			document.documentElement.style.setProperty('--img','url("blue-chevron.jpg")');
+		case 2:
+			
+			document.documentElement.style.setProperty('--light-shade','#AEF78E');
+			document.documentElement.style.setProperty('--medium-shade','#66A182');
+			document.documentElement.style.setProperty('--final-shade','#CAFFB9');
+			document.documentElement.style.setProperty('--text-soft-white','#EFF7F6');
+			document.documentElement.style.setProperty('--grey', '#696969');
+			document.documentElement.style.setProperty('--white','#FFFFFF');
+			document.documentElement.style.setProperty('--black','#000000');
+			document.documentElement.style.setProperty('--img','url("paint-pink-compressed.jpg")');
+
+	}
+
+}
+*/
